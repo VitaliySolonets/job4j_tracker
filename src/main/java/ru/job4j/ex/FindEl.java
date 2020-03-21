60cc0d4999f4a0e40e91dd6106394994762eda1e
VitaliySolonets <Solonets.80@mail.ru>
2020-03-21 23:16:51 +0700

    0.4. Пользовательские исключения.[#258278]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rls = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rls = index;
+            }
+        }
+        if (rls == -1) {
+            throw new ElementNotFoundException("The element is missing");
+        }
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"Ivan", "Petr", "Greg", "Jak"};
+        try {
+            int index = indexOf(value, "Greg");
+            System.out.println("Index of elements is : " + index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
