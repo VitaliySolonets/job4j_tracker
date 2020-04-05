c856e2c2fc847650e76eee38c137c2b4e86b3aa3
VitaliySolonets <Solonets.80@mail.ru>
2020-04-05 22:10:58 +0700

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#258273] 3. Замена if-else-throw на if-throw[#258274] Сдача в кофе машине. [#34741]

---
+++ src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,28 @@
+package ru.job4j.sort;
+/**
+ * Сдача в кофемашине.
+ */
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] cOINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int sum = money - price;
+        while (sum > 0) {
+            for (int i = 0; i < cOINS.length; i++) {
+                if (sum - cOINS[i] < 0) {
+                    continue;
+                } else {
+                    sum = sum - cOINS[i];
+                    rsl[size++] = cOINS[i];
+                    break;
+                }
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -15,7 +15,9 @@ public class ValidateInputTest {
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
         String[] data = {"one", "1"};
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[]{"one", "1"})
+        );
         input.askInt("Enter");
         assertThat(
                 mem.toString(),
@@ -35,7 +37,7 @@ public class ValidateInputTest {
         System.setOut(new PrintStream(mem));
         String[] data = {"7", "1"};
         //эмулируем воод данных пользователя
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(new StubInput(data));
         input.askInt("Enter number", 6);
         assertThat(
                 mem.toString(),
