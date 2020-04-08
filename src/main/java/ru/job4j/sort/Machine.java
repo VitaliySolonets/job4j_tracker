84ffffe38df91998796fa88c8fddf2cb741cce7c
VitaliySolonets <Solonets.80@mail.ru>
2020-04-08 21:03:35 +0700

    Сдача в кофе машине. [#34741] Исправление while

--- src/main/java/ru/job4j/sort/Machine.java
+++ src/main/java/ru/job4j/sort/Machine.java
@@ -6,21 +6,16 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] cOINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int sum = money - price;
-        for (int i = 0; i < cOINS.length; i++) {
-            while (sum > 0) {
-                if (sum - cOINS[i] < 0) {
-                    continue;
-                } else {
-                    sum = sum - cOINS[i];
-                    rsl[size++] = cOINS[i];
-                    break;
-                }
+        for (int coin : coins) {
+            while (sum >= coin) {
+                sum -= coin;
+                rsl[size++] = coin;
             }
         }
         return Arrays.copyOf(rsl, size);
