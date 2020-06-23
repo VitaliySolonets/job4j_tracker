e03a335d67b4b7cc613fe2e96eb05a3ec755e4cc
VitaliySolonets <Solonets.80@mail.ru>
2020-06-23 21:17:59 +0700

    3. Конвертация ArrayList в двухмерный массив [#258239]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,10 +1,7 @@
 package ru.job4j.search;
-
 import java.util.LinkedList;
-
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
-
     /**
      * Метод должен вставлять в нужную позицию элемент.
      * Позицию определять по полю приоритет.
@@ -21,7 +18,6 @@ public class PriorityQueue {
         }
         this.tasks.add(index, task);
     }
-
     public Task take() {
         return tasks.poll();
     }
