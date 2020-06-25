869bf26bb7df66a23193a497dbb78ed2b6cc022b
VitaliySolonets <Solonets.80@mail.ru>
2020-06-25 19:58:04 +0700

    Исправление ошибок

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,7 +12,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() <= element.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             } else index++;
         }
