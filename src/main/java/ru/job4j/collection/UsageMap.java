4195f07b5da069547367fb0bc15918fd2345dc93
VitaliySolonets <Solonets.80@mail.ru>
2020-06-28 20:51:00 +0700

    0. Map, HashMap [#258244]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("solonets.80@mail.ru", "Solonets Vitaly");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
