5a573c3966909defecc96e0abc32900710b72b66
VitaliySolonets <Solonets.80@mail.ru>
2020-06-30 22:52:35 +0700

    3. Банковские переводы. [#258246]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,58 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                users.get(user).add(account);
+            }
+        }
+
+    }
+
+    public User findByPassport(String passport) {
+        for (User value : users.keySet()) {
+            if (passport.equals(value.getPassport())) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        Account rsl = null;
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (requisite.equals(account.getRequisite())) {
+                    rsl = account;
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean result = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            result=true;
+        }
+        return result;
+    }
+}
