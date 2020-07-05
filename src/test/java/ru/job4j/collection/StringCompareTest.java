33ee1e17530d45aee57193a17752091a47ff1199
VitaliySolonets <Solonets.80@mail.ru>
2020-07-05 12:42:25 +0700

    3. Компаратор для строк [#258230]

---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void whenLeftLessRightThanResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Patrov",
+                "Petrov"
+        );
+        assertThat(rsl, lessThan(0));
+    }
+}
